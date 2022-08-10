3941a86bcf518e9d23ee86b3f74d3844fb1a68c4
nacer baaziz <nacerstile@gmail.com>
2022-08-10 04:38:50 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,27 +1,31 @@
 from distutils.core import setup
+import setuptools
+
 setup(
   name = 'NBSapi', 
   packages = ['NBSapi'], 
-  version = '1.0', 
+  version = '1.1', 
   license=' GPL-3.0', 
-  description = 'a python class that helps you to control the sapi5 TTS', 
+  description = 'a python library that helps you to control the sapi5 TTS', 
   author = 'nacer baaziz', 
   author_email = 'nacerbaaziz@ng-space.com', 
-  url = 'https://github.com/baaziznasser/nbsapi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz', 
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  url = 'https://github.com/baaziznasser/nbsapi', 
+  download_url = 'https://github.com/baaziznasser/nbsapi/archive/refs/tags/1.1.tar.gz', 
+  keywords = ['nbsapi', 'NBSapi', 'Sapi', 'tts', 'python text to speech', 'audio', 'screen reader', 'voices', 'sapi5', 'spvoice'], 
   install_requires=[            
           'pywin32',
+          'setuptools',
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GPL-3.0 License', 
+    'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)', 
     'Programming Language :: Python :: 3', 
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
